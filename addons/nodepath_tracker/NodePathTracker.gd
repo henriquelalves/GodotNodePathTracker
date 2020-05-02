fe3553a84c78c090946a3c30e119f7e682679d74
Henrique Alves <henriquelalves@gmail.com>
2020-05-02 10:30:25 -0300

    fix renaming nodes between paths

--- addons/nodepath_tracker/NodePathTracker.gd
+++ addons/nodepath_tracker/NodePathTracker.gd
@@ -43,7 +43,5 @@ func _on_node_added(node : Node):
 			_update_nodes_array()
 
 func _on_node_renamed(node : Node):
-	var index = _nodes.find(node)
-	if (index != -1):
-		nodepaths[index] = get_path_to(node)
-		_update_nodes_array()
+	for node_index in _nodes.size():
+		nodepaths[node_index] = get_path_to(_nodes[node_index])
